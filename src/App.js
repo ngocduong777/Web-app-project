f423b1e8eb990d1b593adec1489ad461c687e081
htpt2910 <hmc2910@gmail.com>
2021-10-04 09:49:25 +0700

    add folder components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     {/* trong file components, moi nguoi tu tao cho minh 1 file jsx de lam nha */}
+     {/* trong file components, moi nguoi tu tao cho minh 1 file jsx de lam */}
      {/* copy tu code cu qua thoi, voi nhung ai ma luc truoc code ngay trong file App.js */}
-     {/* thi can truyen props luc dung component */}
+     {/* thi can truyen props luc dung components */}
      {/* vi du dung component: <Login <prop name(neu co)> = { value }/> */}
     </div>
   );
